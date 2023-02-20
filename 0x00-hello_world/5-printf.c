358009cb380b374a3bccef3786a5ed3d08ea1ad0
Keara Sera <sera.keara@gmail.com>
2023-02-20 09:41:28 -0800

    testing printf 2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - This program uses the printf function
+*main - This program uses the printf function
+*
 * Return: 0
 */
-
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n")
+  printf("with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
