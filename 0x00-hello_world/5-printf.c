d3faca4b412a3b95dd4a5af95d07a58cd5e04f5a
Keara Sera <sera.keara@gmail.com>
2023-02-20 09:53:46 -0800

    restart

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main - This program uses the printf function
+* main - This program uses printf
 *
 * Return: 0
 */
 int main(void)
+{
   printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+  return (0);
 }
